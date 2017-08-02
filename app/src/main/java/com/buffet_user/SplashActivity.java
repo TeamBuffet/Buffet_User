d3c301fd756019eec784adb1a692a24b4de29a2c
Ankit Maurya <ankit.1410025@kiet.edu>
2017-08-03 03:39:57 +0530

    LoginChooser #done

--- app/src/main/java/com/buffet_user/SplashActivity.java
+++ app/src/main/java/com/buffet_user/SplashActivity.java
@@ -1,7 +1,8 @@
 package com.buffet_user;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(SplashActivity.this, LoginChooserActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+
+
     }
 }
