bf02a5c0330107e40c6f0bd6d9b888c86a368a94
Akshay Sharma <akshaybmsa96@gmail.com>
2017-08-12 16:29:04 +0530

    pager update

---
+++ app/src/main/java/com/buffet_user/activity/LoginActivity_Buffet.java
@@ -0,0 +1,26 @@
+package com.buffet_user.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.buffet_user.R;
+
+/**
+ * Created by Ankit on 02/08/17.
+ */
+
+public class LoginActivity_Buffet extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buffet_login);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+    }
+}
---
+++ app/src/main/java/com/buffet_user/activity/SignUpActivity_Common.java
@@ -0,0 +1,8 @@
+package com.buffet_user.activity;
+
+/**
+ * Created by Ankit on 02/08/17.
+ */
+
+public class SignUpActivity_Common {
+}
